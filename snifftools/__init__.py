b9d0be134ba724f14e93980346c26b3ca322cd17
sniff122 <sniff122@sniff122.ga>
2019-01-02 19:56:21 +0000

    Update __init__.py

--- snifftools/__init__.py
+++ snifftools/__init__.py
@@ -1,2 +1,26 @@
 name = "snifftools"
-from snifftools import *
+
+#snifftools
+
+def emailcheck(email : str):
+    if email.find("@") == -1:
+        return(False)
+    else:
+        return(True)
+
+def reverse(text):
+    return(text[::-1])
+
+def join(text1, text2):
+    return(text1 + text2)
+
+def encrypt(message):
+    message = message[::-1]
+    alphabet = "abcdefghijklmnopqrstuvwxyz.!? /:"
+    replacea = "zyxwvutsrqponmlkjihgfedcba!./ ?:"
+    secret = ""
+    message = message.lower()
+    for character in message:		
+        i = alphabet.index(character)
+        secret = secret + replacea[i]
+    return(secret)
